efdcef9362b7c37034af55297e2367ec1d38003e
nguyentubadat <nguyentubadat833@gmail.com>
2024-12-04 14:44:34 +0700

    add base layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UContainer>
+    <UiHeader/>
+    <div>
+      <NuxtPage/>
+    </div>
+    <UiFooter/>
+  </UContainer>
+</template>
+
+<style scoped>
+
+</style>
